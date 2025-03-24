29be9554d8d397733412e7fab47862f3b8b76b54
Soravit Polpraprut <ten7009@gmail.com>
2025-03-24 18:37:03 +0700

    Added config for Vercel

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const cookieParser=require('cookie-parser');
+"Once upon a time, three happy birds lived in a warm and lush forest.
 
-const dotenv = require('dotenv');
-const carbookings = require ('./routes/carbookings');
-const providers = require ('./routes/providers');
-const app = express();
-const auth = require('./routes/auth');
+The birds had no names, though the creatures of the forest would call them Big Bird, Long Bird, and Small Bird.
 
-//Load env vars
-dotenv.config({ path: "./config/config.env" });
+Everyone lived freely and happily together in the peaceful forest. The birds wanted the forest to be safe more than anyone else, that way creatures from the outside could visit and have fun.
 
-//Connect to database
-connectDB();
+It was a warm and sunny afternoon when a stranger visited the peaceful forest. He was a traveler, pioneer, and prophet, but at the same time, he was a nobody. He wanted to enter the forest, but the birds found him suspicious, so they didn't let him in. The stranger, angered, told the birds as he left:
 
+“Soon, a tragedy will befall the forest. It will be tainted with misdeeds and evil, and there will be constant conflict. The tragedies will only stop when a terrible beast appears and devours everything. The sun and moon will shine upon the forest no longer; the forest will never go back to what it was before.”
 
-const PORT = process.env.PORT || 5003;
+The birds worried about the prophecy. What if conflicts really break out and a beast shows up to harm the forest just like the man said? That's when the birds decided to become the guardians of the forest. They loved this forest more than anyone else did, so they wanted to protect the creatures living in the forest and keep the peace.
 
-//Body Parser
-app.use(express.json());
+Big Bird, with its many eyes, decided to watch over the forest for intruders. For Big Bird's eyes could see very far, and see things we cannot see. Then somebody said, “But what if the beast shows up in the middle of the night, when the sky is dark and everyone is asleep?” Worried, Big Bird burned all of its feathers to make an everlasting lamp. Now the creatures of the forest were under Big Bird's constant watch, day and night.
 
-//Cookie Parser
-app.use(cookieParser());
+To keep the forest's peace, Long Bird weighed the sins of those who entered the forest. For Long Bird's scale was fair and just, and could weigh any and all kinds of sin. Then somebody said, “But what if the scale doesn't tip in either direction someday?” Worried, Long Bird made a scale that always tipped to one side. That way every judgement would yield a clear verdict.
 
-app.use('/api/v1/auth', auth)
+Small Bird decided to punish the creatures' wrongdoings with its beak. Then somebody said, “But your beak is so small, no one would find it hurtful.” Worried, Small Bird tore its mouth wide so it could swallow any creature in one bite. 
 
-app.use('/api/v1/carbookings',carbookings);
+No one could visit the forest anymore, and bad rumors about the forest began to spread outside. “I heard terrible things happen to any creature that enters the forest.”
 
-app.use('/api/v1/providers', providers)
+“The Big Bird is always watching the creatures of the forest, there's no freedom.”
+“The Long Bird's scale isn't fair at all.”
+“The Small Bird's punishment is so scary.”
+The birds got upset. “We work so hard to protect the forest; why doesn't anyone appreciate our effort?”
 
-const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+Fewer and fewer visitors came to the forest, more and more creatures were complaining, fights broke out daily, and yet the birds continued to work even harder to make the forest peaceful again, but...
 
-//Handle unhandled promise rejections
-process.on('unhandledRejected',(err,promise)=>{
-  console.log(`Error: ${err.message}`);
-  //Close server & exit process
-  server.close(()=>process.exit(1));
+“No, this forest is so vast and wide, the three of us aren't enough to protect all of it.”
+“But there is no creature to protect this forest but us.”
+“If we combined our power, we could be stronger.”
+The day when Big Bird's eyes that could see hundreds of kilometers away, Long Bird who could judge any sin, and Small Bird's mouth that could devour everything united into one, darkness fell upon the forest. Creatures screamed in terror and fear at the sight of the bird, and the forest soon fell into mayhem.
 
-})
+Then, in the middle of all the chaotic cries, someone shouted:
+
+“It's the beast! A big, scary monster lives in the black, dusky forest!”
+
+The beast is here? The three birds, now as one, looked around but there was no one to be seen. The bird began to prowl the forest, looking for the monster. The forest would be in trouble if the beast really showed up.
+
+But there was nothing. There were no creatures, no sun and moon, and no beast. All that was left was just a bird and the Black Forest.
+
+Since then, there were only nights dark as pitch.
+
+Rumors say that there is a terrible beast in the Black Forest where no one lives."
